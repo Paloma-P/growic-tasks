44d078bc018b7c280dd3b6a10e768b0e1d0cc5c5
Paloma-P <67731337+Paloma-P@users.noreply.github.com>
2022-08-22 10:36:41 +0100

    Add files via upload

---
+++ mappings.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+//https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+
+
+contract UserBalance {
+    
+    address public owner;
+   
+    mapping(address => uint) public balance;
+    
+    function deposit (uint256 amount) public payable {
+        balance[msg.sender] += amount ;
+    }
+    function checkBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+}
